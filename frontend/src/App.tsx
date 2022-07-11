5281ac814ed23fe2e1d5e9627df8bf88f704cd73
Anderson Silva <andersonvieira7777@gmail.com>
2022-07-11 18:09:04 -0300

    First button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import NotificationButton from "./components/notificationButton"
 
 
 function App() {
-  return(
-    <h1>Bora começar o app </h1>
+  return (
+    <>
+      <h1>Bora começar o app </h1>
+      <NotificationButton />
+    </>
   )
-  
+
 }
 
 export default App
