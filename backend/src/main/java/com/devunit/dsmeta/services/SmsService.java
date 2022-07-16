7ad8732b2f20bee8c8e97fb63670c54231769513
Anderson Silva <andersonvieira7777@gmail.com>
2022-07-15 21:56:35 -0300

    config class SmsService message text

--- backend/src/main/java/com/devunit/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devunit/dsmeta/services/SmsService.java
@@ -31,6 +31,12 @@ public class SmsService {
 
         Sale sale = saleRepository.findById(saleId).get();
 
+        String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+
+        String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+                + "com um total de R$ " + String.format("%.2f", sale.getAmount());
+
+
         Twilio.init(twilioSid, twilioKey);
 
         PhoneNumber to = new PhoneNumber(twilioPhoneTo);
